ca1e8a43052fe8aec5709a5e6c8f371716caa90c
Chanthai1102 <114852768+Chanthai1102@users.noreply.github.com>
2023-06-30 23:53:45 +0700

    Update notification.blade.php

--- resources/views/Backend/notification.blade.php
+++ resources/views/Backend/notification.blade.php
@@ -4,26 +4,6 @@
     <!-- Content wrapper -->
     <div class="content-wrapper p-4">
         <div class="container-xxl flex-grow-1 container-p-y row" id="callservce">
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <h5 class="card-header">Call Service</h5>
-                    <div class="card-body">
-                        <div class="p-4">
-                            <p class="text-center fs-large fw-bold" id="nametable">Table 001</p>
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <button
-                                type="button"
-                                class="btn btn-primary"
-                                data-bs-toggle="modal"
-                                data-bs-target="#exLargeModal"
-                            >
-                                Accept
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     <div class="content-backdrop fade"></div>
@@ -33,7 +13,7 @@
 
 <script src="https://js.pusher.com/6.0/pusher.min.js"></script>
 <script>
-
+    var listTable = [];
 
     //suscribing to pusher channel
     Pusher.logToConsole = false;
@@ -48,11 +28,50 @@
     var channel = pusher.subscribe('events');
     channel.bind('App\\Events\\RealTimeMessage',(d) => {
         if(d){
-            var nametable = (d.message);
-            var div = document.getElementById('callservce');
-            div.innerHTML += '<div class="col-md-4"> <div class="card mb-4"> <h5 class="card-header">Call Service</h5> <div class="card-body"> <div class="p-4"> <p class="text-center fs-large fw-bold" id="nametable"></p></div> <div class="d-flex justify-content-end"> <buttontype="button"class="btn btn-primary"data-bs-toggle="modal"data-bs-target="#exLargeModal" >Accept </button> </div> </div> </div> </div>';
-            document.getElementById('nametable').innerHTML = nametable;
+            if(listTable.includes(d.message)){
+                alert("Exist Table");
+            }else{
+                var nametable = (d.message);
+                listTable.push(d.message);
+                tableCard();
+            }
         }
     });
+
+
+    function onAcceptClick(table){
+        //console.log(table);
+        console.log(listTable);
+        var index = listTable.indexOf("'"+table+"'");
+        console.log(index);
+        if (index > -1) { // only splice array when item is found
+            listTable.splice(index, 1); // 2nd parameter means remove one item only
+        }
+        tableCard();
+    }
+
+
+    function tableCard(){
+        $('#callservce').html("");
+        for(var i=0; i<listTable.length; i++){
+            $('#callservce').append(
+                '<div class="col-md-4">' +
+                '<div class="card mb-4">' +
+                '    <h5 class="card-header">Call Service</h5>' +
+                '    <div class="card-body">' +
+                '        <div class="p-4">' +
+                '            <p class="text-center fs-large fw-bold">Table '+listTable[i]+'</p>' +
+                '        </div>' +
+                '        <div class="d-flex justify-content-end">' +
+                '            <button type="button"  class="btn btn-primary" onClick="onAcceptClick('+listTable[i]+')">' +
+                '                Accept' +
+                '            </button>' +
+                '        </div>' +
+                '    </div>' +
+                '</div>' +
+                '</div>'
+            );
+        }
+    }
 </script>
 @endsection
